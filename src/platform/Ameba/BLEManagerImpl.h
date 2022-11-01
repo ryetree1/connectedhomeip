67db34ba17e506bb9f1fa4ab9ad20990f773a345
Ryan <rarbour@leviton.com>
2022-11-01 14:54:01 -0400

    Restart Advertising when disconnected if not enrolled

--- src/platform/Ameba/BLEManagerImpl.h
+++ src/platform/Ameba/BLEManagerImpl.h
@@ -161,7 +161,7 @@ private:
     void AddConnection(uint8_t connectionHandle);
 
     BLEManagerImpl::CHIPoBLEConState * GetConnectionState(uint8_t connectionHandle, bool allocate);
-    static CHIP_ERROR ble_svr_gap_msg_event(void * param, T_IO_MSG * p_gap_msg);
+    CHIP_ERROR ble_svr_gap_msg_event(void * param, T_IO_MSG * p_gap_msg);
     static CHIP_ERROR ble_svr_gap_event(void * param, int cb_type, void * p_cb_data);
     static CHIP_ERROR gatt_svr_chr_access(void * param, T_SERVER_ID service_id, TBTCONFIG_CALLBACK_DATA * p_data);
     static int ble_callback_dispatcher(void * param, void * p_cb_data, int type, T_CHIP_BLEMGR_CALLBACK_TYPE callback_type);
